b8ceec681d3551d0c07d7e6fd9cd796b0da8ce94
lixu <1336840283@qq.com>
2019-04-22 15:10:58 +0800

    OJ前台框架

---
+++ src/main/java/com/oj/controller/system/IndexController.java
@@ -0,0 +1,16 @@
+package com.oj.controller.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+    @RequestMapping("/")
+    //返回index.html页面
+    public String index(HttpServletRequest request) {
+        return "index";
+    }
+}
