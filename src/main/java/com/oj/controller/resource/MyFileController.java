7926549951ce8139eed0b6ed5cfe5ca976b8d947
zhengtong <1348190999@qq.com>
2019-05-08 14:16:11 +0800

    新增若文件不存在，点击下载时会提示。

--- src/main/java/com/oj/controller/resource/MyFileController.java
+++ src/main/java/com/oj/controller/resource/MyFileController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -49,65 +50,20 @@ public class MyFileController {
     public void downloadFile(HttpServletRequest request, HttpServletResponse response)
     {
         String id = request.getParameter("id");
-        //System.out.println("id : " + id);
+        System.out.println("id : " + id);
         myfileService.downloadFile(id, response);
     }
-    /*
-    //返回实验列表页面
-    @RequestMapping("/")
-    public String index(ModelMap modelMap, HttpServletRequest request) {
-        return "exam/exper";
-    }
-
-    //返回实验详情页面
-    @RequestMapping("experDetail")
-    public String experDetail(HttpServletRequest request) {
-        return "exam/experDetail";
-    }
-
-    //返回题目详情页面
-    @RequestMapping("/problemDetails")
-    public String problemDetails( HttpServletRequest request) {
-        return "exam/problemDetails";
-    }
-
-    //通过学生所在班级获取全部实验信息接口
-    @PostMapping("/getAllExper")
+    @PostMapping("/checkFileExistence")
     @ResponseBody
-    public List<Map> getAllExper(HttpServletRequest request){
-        String account = request.getSession().getAttribute("user_class").toString();
-        return testService.getExperMaplist(account);
+    public Map<String, String>checkFileExistence(HttpServletRequest request) {
+        System.out.println("come in check");
+        Map<String, String> map = new HashMap<>();
+        if (myfileService.checkFileExistence(request.getParameter("id"))) {
+            map.put("flag", "1");
+            return map;
+        } else {
+            map.put("flag", "0");
+            return map;
+        }
     }
-
-    //通过实验id和学生学号获取实验成绩
-    @PostMapping("/getSubmitState")
-    @ResponseBody
-    public List<Map> getSubmitState(@RequestBody Map<String, String> param, HttpServletRequest request){
-        List<Map> list =  testService.getSubmitState(request.getSession().getAttribute("user_id").toString(),param);
-        return list;
-    }
-
-    //获取一条实验或考试的详细信息
-    @PostMapping("/getTestDetail")
-    @ResponseBody
-    public List<Map> getTestDetail(HttpServletRequest request){
-        List<Map> list =  testService.getTestDetail(request.getParameter("tid"));
-        return list;
-    }
-
-    @PostMapping("getProblemDetails")
-    @ResponseBody
-    public List<Map> getProblemDetails(HttpServletRequest request){
-        List<Map> p = testService.getProblemDetails(request.getParameter("id"));
-        return p;
-    }
-
-    //获取提交状态分类
-    @PostMapping("getSubmitType")
-    @ResponseBody
-    public List<Map> getSubmitType(HttpServletRequest request){
-        List<Map> p = testService.getSubmitType();
-        return p;
-    }*/
-
 }
