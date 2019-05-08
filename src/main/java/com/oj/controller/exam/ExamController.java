89024a219f4e9c537c215baec7fc197cb967899e
LeighZh <1803124917@qq.com>
2019-05-08 23:18:05 +0800

    开发考试功能

--- src/main/java/com/oj/controller/exam/ExamController.java
+++ src/main/java/com/oj/controller/exam/ExamController.java
@@ -1,16 +1,20 @@
 package com.oj.controller.exam;
 
+import com.oj.mapper.system.LoginMapper;
 import com.oj.service.exam.TestService;
+import com.oj.service.system.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +31,8 @@ public class ExamController {
 
     @Autowired
     private TestService testService;
+    @Autowired
+    private LoginService loginService;
 
     //返回考试列表页面
     @RequestMapping("/")
@@ -50,7 +56,51 @@ public class ExamController {
     @PostMapping("/getAllExam")
     @ResponseBody
     public List<Map> getAllExper(HttpServletRequest request){
-        String account = request.getSession().getAttribute("user_class").toString();
-        return testService.getExamMaplist(account);
+        String cid = request.getSession().getAttribute("user_class").toString();
+        String sid = request.getSession().getAttribute("user_id").toString();
+        return testService.getExamMaplist(sid,cid);
+    }
+
+    //将初次登陆考试的ip进行记录
+    @PostMapping("/recordIP")
+    @ResponseBody
+    public Map<String, String> recordIP(HttpServletRequest request){
+        String sid = request.getSession().getAttribute("user_id").toString();
+        String tid = request.getParameter("tid");
+        String first_ip = request.getParameter("ip");
+        Map<String, String> map = new HashMap<>();
+        try {
+            testService.saveIP(tid,sid,first_ip);
+            map.put("flag", "1");
+            return map;
+        }catch (Exception e){
+            map.put("flag", "0");
+            log.error(e.getMessage());
+            return map;
+        }
+    }
+
+    //获取可参与考试的ip段
+    @PostMapping("/getTestIp")
+    @ResponseBody
+    List<Map> getTestIp(HttpServletRequest request){
+        return testService.getTestIp(request.getParameter("tid"));
+    }
+
+    //获取可参与考试的班级
+    @PostMapping("/getTestClass")
+    @ResponseBody
+    List<Map>   getTestClass(@RequestBody Map<String, String> loginInfo,HttpServletRequest request){
+        String cid = "";
+        List<Map<String, String>> userList = loginService.getUserByLoginInfo(loginInfo);
+        //通过用户登录名和密码查询数据库看是否有该用户
+        if (userList.size()==1)
+           cid = userList.get(0).get("class_id");
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("class_id", cid);
+        List<Map> test = testService.getTestClass();
+        test.add(map);
+        System.out.println(test.contains(map));
+        return test;
     }
 }
--- src/main/resources/static/js/OJ/exam/experDetail.js
+++ src/main/resources/static/js/OJ/exam/experDetail.js
@@ -4,11 +4,21 @@ var tid = getParam("id");
 var setedTestInfo = false;
 var setedProblemInfo = false;
 $(document).ready(function () {
+    $("#code").height($(window).innerHeight() * 0.75)
     getSubmitType();
-    getInfo();
+    getUserIP(function(ip){
+        getInfo(ip);
+        if(getParam("isSaveIp") !="undefined"){
+            $("#breadList").html("考试列表");
+            $("#breadList").attr("href","/exam/")
+            $("#breadDetail").html("考试");
+        }
+        getParam("isSaveIp") == 1 ? savaFirstIP(ip,getParam("id")) : null;
+    });
+
 });
 
-function getInfo(){
+function getInfo(ip){
     $.ajax({
         type: "POST",
         url: "/experiment/getSubmitState",
@@ -21,29 +31,41 @@ function getInfo(){
                 "tid" : tid
             }),
         success:function (result) {
-            submitTable.empty();
+            //console.log(result)
+            var list_map = new Array();
+            var list_map_submit = new Array();
             var problemsId = new Array();
+            var score = new Array();
             var pName = new Array();
             var isAC = new Array();
             var pNum = -1;
-            var ACNum = -1;
+            var ACNum = 0;
+            var submitNum = 0;
             $.each(result,function(index,value){
+                value.submit_date = formatTime(value.submit_date)
                 if(problemsId.indexOf(value.id) == -1){
                     pNum++;
                     problemsId[pNum] = value.id;
                     pName[pNum] = value.name;
+                    score[pNum] = value.score;
                     isAC[pNum] = false;
                 }
                 var idIndex= problemsId.indexOf(value.id)
                 if(value.submit_state == "Accepted" && idIndex != -1 ){
                     isAC[idIndex] = true;
                 }
-                if(value.submit_state != null && value.score != null)
-                setSubmitInfo(value.id,value.name,value.submit_state,value.submit_language,value.submit_time,value.submit_date,value.submit_memory,value.submit_code_length,value.submit_code);
+                if(value.submit_state != null && value.score != null){
+                    list_map_submit.push({id: value.id,name: value.name,state: value.submit_state,memory:value.submit_memory + "kb",language:value.submit_language,time:value.submit_time + "ms",submitTime:value.submit_date,length:value.submit_code_length + "b",code:value.submit_code})
+                    submitNum++;
+                }
             })
+            setSubmitInfo(list_map_submit,tid);
             if(!setedProblemInfo){
-                setProblemsInfo(problemsId,pName,isAC);
-                setedProblemInfo = true;
+                for ( var i = 0; i < problemsId.length; i++) {
+                    list_map.push({id: problemsId[i], name: pName[i], isAc: isAC[i],score:score[i]});
+                }
+                setProblemsInfo(list_map,tid);
+                //setedProblemInfo = true;
             }
             if(!setedTestInfo){
                 for(var i = 0; i <= isAC.length; i++){
@@ -51,9 +73,7 @@ function getInfo(){
                         ACNum++;
                     }
                 }
-                if(ACNum == -1)
-                    ACNum = 0;
-                setTestInfo(tid,((ACNum / pNum) * 100).toFixed(2)) ;
+                setTestInfo(tid,((ACNum /++pNum) * 100).toFixed(2),ip) ;
                 setedTestInfo = true;
             }
         }
@@ -74,9 +94,9 @@ function getSubmitType(){
     })
 }
 
-function setTestInfo(id,progress){
-    $("#status").html(getParam("isEnd")== 1 ? "已结束" : "正在进行");
-    $("#status").addClass(getParam("isEnd")== 1 ? "label-default" : "label-primary");
+function setTestInfo(id,progress,ip){
+    $("#status").html(getParam("testState")== 0 ? "已结束" : "正在进行");
+    $("#status").addClass(getParam("testState")== 0 ? "label-default" : "label-primary");
     progress += "%"
     $("#progress").html(progress);
     $("#progrocessBar").css("width", progress);
@@ -93,69 +113,169 @@ function setTestInfo(id,progress){
             $("#admin").html(result[0].admin);
             $("#end").html(formatTime(result[0].end));
             $("#start").html(formatTime(result[0].start));
+            //console.log()
+            // if(getParam("isSaveIp") != "undefined" &&getParam("testState") == 1 &&result[0].only_ip == "on" && result[0].is_ip == "on" ){
+            //     $.ajax({
+            //         type: "POST",
+            //         url: "/exam/getTestIp",
+            //         dataType: "json",
+            //         data:{
+            //             "tid" : id
+            //         },
+            //         success:function (result) {
+            //             var ipArray = new Array();
+            //             for(var i = 0; i< result.length; i++){
+            //                 if(result[i].ip.charAt(result.length - 1) == '.')
+            //                     result[i].ip = result[i].ip.substring(0,length - 1);
+            //                 ipArray[i] = result[i].ip;
+            //                 console.log("允许进入考试的ip段:" + result[i].ip)
+            //             }
+            //             console.log("用户ip：" + ip)
+            //             if(!checkIP(ip,ipArray)){
+            //                 hint();
+            //             }
+            //         }
+            //     })
+            // }
         }
     })
 }
 
-function setProblemsInfo(id,name,isAC) {
-    // isAC = isAC[i] == 1 ? "AC" : "NA";
-    // var dataTable = $("#problems");
-    // if ($.fn.dataTable.isDataTable(dataTable)) {
-    //     dataTable.DataTable().destroy();
-    // }
-    // dataTable.DataTable({
-    //     "serverSide": false,
-    //     "autoWidth" : false,
-    //     "bSort": false,
-    //     "data" : result,
-    //     "columns" : [{
-    //         "data" : "id"
-    //     },{
-    //         "data" : "name"
-    //     },{
-    //         "data" : "isAC",
-    //         "defaultContent" : "NA"
-    //     }],
-    //     "columnDefs": [{
-    //         "render" : function(data, type, row) {
-    //            var problemItem = "<td class=\\\"project-actions\\\"> <a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+row.id+"\")' class=\"btn btn-white btn-sm\"><i class=\"fa fa-folder\"></i> 查看 </a></td>";return a;
-    //             return problemItem;
-    //         },
-    //         "targets" :7
-    //     }]
-    // });
-    problemTable.empty();
-    for(var i = 0; i < id.length; i++){
-        var problemItem = "<tr>";
-        problemItem += "<td ><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")'>" + id[i] + "</a></td>";
-        problemItem += "<td><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")'>" + name[i] + "</a></td>";
-        var success =  "<td><span class=\"label label-primary\"><i class=\"fa fa-check\"></i>AC</span> </td>";
-        var wrong = "<td><span class=\"label label-default\"><i class=\"fa fa-frown-o\"></i>NA</span></td>";
-        problemItem += isAC[i] == 1 ? success : wrong;
-        problemItem += "<td class=\\\"project-actions\\\"> <a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")' class=\"btn btn-white btn-sm\"><i class=\"fa fa-folder\"></i> 查看 </a></td>";
-        //problemItem += "<td class='project-actions' onclick='setId(\""+id[i]+"\")'data-toggle='modal' data-target='#myModal5' href='problemDetails'><i class=\"fa fa-folder\"></i> 查看 </td>"
-        problemItem += "</tr>";
-        problemTable.append(problemItem);
+function setProblemsInfo(result,testId) {
+    console.log(result)
+    var testState = getParam("testState");
+    var dataTable = $("#problems");
+    if ($.fn.dataTable.isDataTable(dataTable)) {
+        dataTable.DataTable().destroy();
     }
+    dataTable.DataTable({
+        "serverSide": false,
+        "autoWidth" : false,
+        "bSort": false,
+        "data" : result,
+        "columns" : [{
+            "data" : "id"
+        },{
+            "data" : "name"
+        },{
+            "data" : "score"
+        },{
+            "data" : "isAC",
+            "defaultContent" : "NA"
+        }],
+        "columnDefs": [{
+            "render" : function(data, type, row) {
+                return testState == 0&&(getParam("isSaveIp")!="undefined") ? "<td>" + row.id + "</td>" : "<td ><a style=\"color:grey;\" onclick='getProblem(\""+row.id+"\",\""+testId+"\")'>" + row.id + "</a></td>";
+            },
+            "targets" :0
+        },{
+            "render" : function(data, type, row) {
+                return testState == 0&&(getParam("isSaveIp")!="undefined") ? "<td>" + row.name + "</td>" : "<td ><a style=\"color:grey;\" onclick='getProblem(\""+row.id+"\",\""+testId+"\")'>" + row.name + "</a></td>";
+            },
+            "targets" :1
+        },{
+            "render" : function(data, type, row) {
+                var success =  "<td><span class=\"label label-primary\"><i class=\"fa fa-smile-o\"></i>AC</span> </td>";
+                var wrong = "<td><span class=\"label label-default\"><i class=\"fa fa-frown-o\"></i>NA</span></td>"
+                return row.isAc == true ? success : wrong;
+            },
+            "targets" :3
+        }, {
+                "render" : function(data, type, row) {
+                    var problemItem = testState == 0&&(getParam("isSaveIp")!="undefined") ? "<td class=\"project-actions\"> <a class=\"btn btn-white btn-sm\"><i class=\"fa fa-folder\"></i> 查看 </a></td>\";" :"<td class=\"project-actions\"> <a  onclick='getProblem(\""+row.id+"\",\""+testId+"\")' class=\"btn btn-white btn-sm\"><i class=\"fa fa-folder\"></i> 查看 </a></td>";
+                  // console.log((getParam("isSaveIp")=="undefined"))
+                    return problemItem;
+                },
+                "targets" :4
+            }],
+
+
+    });
+
+    // problemTable.empty();
+    // for(var i = 0; i < id.length; i++){
+    //     var problemItem = "<tr>";
+    //     problemItem += testState == 0 ? "<td>" + id[i] + "</td>" : "<td ><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")'>" + id[i] + "</a></td>";
+    //     problemItem += testState == 0 ?"<td>" + name[i] + "</td>"  : "<td><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")'>" + name[i] + "</a></td>";
+    //     var success =  "<td><span class=\"label label-primary\"><i class=\"fa fa-smile-o\"></i>AC</span> </td>";
+    //     var wrong = "<td><span class=\"label label-default\"><i class=\"fa fa-frown-o\"></i>NA</span></td>";
+    //     problemItem += isAC[i] == 1 ? success : wrong;
+    //     problemItem += testState == 0 ? "<td class=\"project-actions\"> <a class=\"btn btn-white btn-sm\"><i class=\"fa fa-folder\"></i> 查看 </a></td>\";" :"<td class=\"project-actions\"> <a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")' class=\"btn btn-white btn-sm\"><i class=\"fa fa-folder\"></i> 查看 </a></td>";
+    //     //problemItem += "<td class='project-actions' onclick='setId(\""+id[i]+"\")'data-toggle='modal' data-target='#myModal5' href='problemDetails'><i class=\"fa fa-folder\"></i> 查看 </td>"
+    //     problemItem += "</tr>";
+    //     problemTable.append(problemItem);
+    // }
 }
 
-function setSubmitInfo(id,name,state,language,time,submitTime,memory,length,code){
-    var submitItem = "<tr>";
-    var success ="<td><span class=\"label label-primary\"><i class=\"fa fa-check\"></i>AC</span> </td>";
-    var wrong = "<td><span class=\"label label-default\"><i class=\"fa fa-frown-o\"></i>" + state + "</span></td>";
-    console.log(code)
-    submitItem += "<td ><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id+"\")'>" + id + "</td>";
-    submitItem += "<td><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id+"\")'>" + name + "</td>";
-    submitItem += state == "Accepted" ? success : wrong;
-    submitItem += "<td>" + language + "</td>";
-    submitItem += "<td>" + time+ "ms</td>";
-    submitItem += "<td>" +memory + "kb</td>";
-    submitItem += "<td>" + length + "b</td>";
-    submitItem += "<td>" + formatTime(submitTime) + "</td>";
-// <button type='button' class='btn btn-primary' onclick='setId(\""+row.id+"\")'data-toggle='modal' data-target='#myModal5' href='problemDetails' style='margin-right:15px; margin-bottom: -1px;'><i class='fa fa-list'></i>&nbsp;详情</button>"
-    submitItem += "<td class=\"project-actions btn btn-white btn-sm>\" onclick='setCode(\""+escape(code)+"\")' data-toggle='modal' data-target='#myModal5'><i class=\"fa fa-folder\"></i> 查看 </a></td>";
-    submitItem += "</tr>";
-    submitTable.append(submitItem);
+function setSubmitInfo(result,testId){
+    console.log(result)
+    var testState = getParam("testState")
+    var dataTable = $("#submit");
+    if ($.fn.dataTable.isDataTable(dataTable)) {
+        dataTable.DataTable().destroy();
+    }
+    dataTable.DataTable({
+        "serverSide": false,
+        "autoWidth" : false,
+        "bSort": false,
+        "data" : result,
+        "columns" : [{
+            "data" : "id",
+        },{
+            "data" : "name",
+        },{
+            "data" : "state",
+        },{
+            "data" : "language",
+        },{
+            "data" : "time",
+        },{
+            "data" : "memory",
+        },{
+            "data" : "length",
+        },{
+            "data" : "submitTime",
+        }],
+        "columnDefs": [{
+            "render" : function(data, type, row) {
+                var submitItem = "<td class=\"project-actions\"> <a class=\"btn btn-white btn-sm\" onclick='setCode(\""+escape(row.code)+"\")' data-toggle='modal' data-target='#myModal5'><i class=\"fa fa-folder\"></i> 查看 </a></td>";
+                return submitItem;
+            },
+            "targets" :8
+        },{
+            "render" : function(data, type, row) {
+                var success ="<td><span class=\"label label-primary\"><i class=\"fa fa-smile-o\"></i>AC</span> </td>";
+                var wrong = "<td><span class=\"label label-default\"><i class=\"fa fa-frown-o\"></i>" + row.state + "</span></td>";
+                return row.state == "Accepted" ? success : wrong;
+            },
+            "targets" :2
+        },{
+            "render" : function(data, type, row) {
+                return testState == 0&&(getParam("isSaveIp")!="undefined") ? "<td>" + row.id + "</td>" : "<td ><a style=\"color:grey;\" onclick='getProblem(\""+row.id+"\",\""+testId+"\")'>" + row.id + "</a></td>";
+            },
+            "targets" :0
+        },{
+            "render" : function(data, type, row) {
+                return testState == 0&&(getParam("isSaveIp")!="undefined") ? "<td>" + row.name + "</td>" : "<td ><a style=\"color:grey;\" onclick='getProblem(\""+row.id+"\",\""+testId+"\")'>" + row.name + "</a></td>";
+            },
+            "targets" :1
+        }]
+    });
+//     var submitItem = "<tr>";
+//     var success ="<td><span class=\"label label-primary\"><i class=\"fa fa-check\"></i>AC</span> </td>";
+//     var wrong = "<td><span class=\"label label-default\"><i class=\"fa fa-frown-o\"></i>" + state + "</span></td>";
+//     submitItem += testState == 0 ? "<td>" + id + "</td>" : "<td ><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")'>" + id[i] + "</a></td>";
+//     submitItem += testState == 0 ?"<td>" + name + "</td>"  : "<td><a data-toggle='modal' data-target='#myModal6' href='problemDetails' onclick='setId(\""+id[i]+"\")'>" + name[i] + "</a></td>";
+//     submitItem += state == "Accepted" ? success : wrong;
+//     submitItem += "<td>" + language + "</td>";
+//     submitItem += "<td>" + time+ "ms</td>";
+//     submitItem += "<td>" +memory + "kb</td>";
+//     submitItem += "<td>" + length + "b</td>";
+//     submitItem += "<td>" + formatTime(submitTime) + "</td>";
+// // <button type='button' class='btn btn-primary' onclick='setId(\""+row.id+"\")'data-toggle='modal' data-target='#myModal5' href='problemDetails' style='margin-right:15px; margin-bottom: -1px;'><i class='fa fa-list'></i>&nbsp;详情</button>"
+//     submitItem += "<td class=\"project-actions btn btn-white btn-sm>\" onclick='setCode(\""+escape(code)+"\")' data-toggle='modal' data-target='#myModal5'><i class=\"fa fa-folder\"></i> 查看 </a></td>";
+//     submitItem += "</tr>";
+//     submitTable.append(submitItem);
 }
 
 //获取url中的参数
@@ -190,7 +310,104 @@ function resetForm() {
     getInfo();
 }
 
-//设置题目详情页的id，用于获取数据
-function setId(id){
-    $('#proid').val(id);
+// //设置题目详情页的id，用于获取数据
+// function setId(id){
+//     //$('#proid').val(id);
+//     window.open("/practice/showProblemInf?proId="+id+"&proAcPercentage="+0+"&proAcNum="+0+"&proSubNum="+0,"_blank"); //从用户的使用逻辑上减轻服务器负担（既保留原题目集页面，可以一定程度上减少用户对服务器的请求
+//
+// }
+
+function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+            iceServers: []
+        }),
+        noop = function() {},
+        localIPs = {},
+        ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+        key;
+
+    function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+
+    //create a bogus data channel
+    pc.createDataChannel("");
+
+    // create offer and set local description
+    pc.createOffer().then(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+
+        pc.setLocalDescription(sdp, noop, noop);
+    }).catch(function(reason) {
+        // An error occurred, so handle the failure to connect
+    });
+
+    //sten for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
 }
+
+
+//将初次登陆考试的ip进行记录
+function savaFirstIP(ip,tid){
+    $.ajax({
+        type: "POST",
+        url: "/exam/recordIP",
+        dataType: "json",
+        data:{
+            "ip" : ip,
+            "tid" : tid,
+        },
+        success: function (result) {
+            if(result.flag == 1 ){
+
+            }else{
+                swal("登入失败！", result.message, "error");
+            }
+        }
+    })
+}
+
+//如果用户登陆ip不符合，弹出提示窗
+function hint(){
+    swal({
+        title: "考试地址错误！",
+        type: "error",
+        text: "请按照老师要求到指定机房进行考试",
+        confirmButtonText:"确认",
+    },function (isConfirm) {
+        if(isConfirm)
+            window.location.href="/exam/";
+    });
+}
+
+//获取题目信息
+function getProblem(id,testId){
+    $.ajax({
+        type: "POST",
+        url: "/experiment/getProblemDetails",
+        dataType: "json",
+        async:false,
+        data:{
+            "id" : id
+        },success:function (result){
+           var ac = (result[0].AC_number);
+           var sum = (result[0].submit_number);
+           var rate = ((result[0].AC_number / result[0].submit_number) * 100).toFixed(2);
+           window.open("/practice/showProblemInf?proId="+id+"&proAcPercentage="+rate+"&proAcNum="+ac+"&proSubNum="+sum+"&testID=" + testId,"_blank"); //从用户的使用逻辑上减轻服务器负担（既保留原题目集页面，可以一定程度上减少用户对服务器的请求
+
+        }
+    })
+}
+
+
+
+
