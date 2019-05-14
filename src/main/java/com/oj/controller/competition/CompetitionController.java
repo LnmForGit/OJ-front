b9bb8a0238339a2523b6a4e074d20bdac4bd2fca
Ly <786116330@qq.com>
2019-05-14 09:04:13 +0800

    竞赛+讨论区点赞优化

--- src/main/java/com/oj/controller/competition/CompetitionController.java
+++ src/main/java/com/oj/controller/competition/CompetitionController.java
@@ -40,13 +40,92 @@ public class CompetitionController {
     @PostMapping("/getAComp")
     @ResponseBody
     public List<Map> getAComplist() {
-        List<Map> list = competitionService.getACompMaplist();
 
 
-        return list;
+
+        return competitionService.getACompMaplist();
     }
 
     //排名信息
+    @PostMapping("/getrankList")
+    @ResponseBody
+    public List<Map> getrankList() {
 
+        return competitionService.getrankList();
+    }
 
+    @RequestMapping("/showmorerank")
+    public String showmorerank(Model model) {
+        Map<String, Object> info = new HashMap<>();
+        info.put("rank",competitionService.showmorerank());
+        model.addAttribute("info", info);
+        return "competition/showrank";
+    }
+
+    @RequestMapping("/showComp/{id}")
+    public String showComp(@PathVariable String id, Model model) {
+        Map<String, Object> info = new HashMap<>();
+        info.put("compinfo",competitionService.showComp(id));
+        info.put("proList",competitionService.Compro(id));
+        model.addAttribute("info", info);
+        return "competition/showComp";
+    }
+    //提交状态信息
+    @PostMapping("/submitStatus")
+    @ResponseBody
+    public List<Map> submitStatus(@RequestBody Map<String, Object> param,HttpServletRequest request) {
+          param.put("user_id",request.getSession().getAttribute("user_id").toString());
+        return competitionService.submitStatus(param);
+    }
+    @PostMapping("/rankListbyId")
+    @ResponseBody
+    public List<Map> rankListbyId(@RequestBody Map<String, Object> param) {
+
+        return competitionService.rankListbyID(param.get("id").toString());
+    }
+    //是否报名
+    @PostMapping("/Isenroll")
+    @ResponseBody
+    public int Isenroll(@RequestBody Map<String,String> param,HttpServletRequest request) {
+        param.put("user_id",request.getSession().getAttribute("user_id").toString());
+        return competitionService.Isenroll(param);
+    }
+    //报名
+    @PostMapping("/enroll")
+    @ResponseBody
+    public boolean enroll(@RequestBody Map<String,String> param,HttpServletRequest request) {
+        System.out.println(param);
+        param.put("user_id",request.getSession().getAttribute("user_id").toString());
+        return competitionService.enroll(param);
+    }
+    //取消报名
+    @PostMapping("/deleteenroll")
+    @ResponseBody
+    public boolean deleteenroll(@RequestBody Map<String,String> param,HttpServletRequest request) {
+        param.put("user_id",request.getSession().getAttribute("user_id").toString());
+        return competitionService.deleteenroll(param);
+    }
+    //报名表
+    @PostMapping("/enrollList")
+    @ResponseBody
+    public List<Map> enrollList(@RequestBody Map<String,String> param) {
+
+        return competitionService.enrollList(param.get("test_id"));
+    }
+    @PostMapping("/invComp")
+    @ResponseBody
+    public String invComp(HttpServletRequest request,@RequestBody Map<String,String> param) {
+        String id = request.getSession().getAttribute("user_id").toString();
+        param.put("user_id", id);
+        return competitionService.RandComp(param);
+
+    }
+    @PostMapping("/CompedList")
+    @ResponseBody
+    public List<Map> CompedList(HttpServletRequest request) {
+        String id = request.getSession().getAttribute("user_id").toString();
+
+        return competitionService.CompedList(id);
+
+    }
 }
